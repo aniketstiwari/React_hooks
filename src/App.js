13c73d061fa68cd5a9e7929d21dac2cb02b11ea9
anikett-rpx <anikett_c@rpxcorp.com>
2020-09-24 11:49:18 +0530

    Hooks & build digital clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+const App = () => {
+
+// useState is a hook in react for maintaining state in component
+// it basically return two values Current_data & function
+// current_data , updated data = initialdata
+  const [count, setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+  }
+  ///////////////////////////////////////////////////////
+
+  let newTime = new Date().toLocaleTimeString();
+  const [ctime, UpdateTime] = useState(newTime);
+
+  const SetTime = () => {
+    newTime  = new Date().toLocaleTimeString();
+    UpdateTime(newTime)
+  }
+
+  /////////////////////////////////////////////////////////////
+
+  const [ctime2, UpdateCTime] = useState(newTime);
+
+  const DigitalTime = () => {
+    newTime  = new Date().toLocaleTimeString();
+    UpdateCTime(newTime);
+  }
+
+  setInterval(DigitalTime, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Time {ctime}</h1>
+      <button onClick={SetTime}>Click me</button>
+      <hr/>
+      <h1>{count}</h1>
+      <button onClick={IncNum}>Click me</button>
+      <hr/>
+      <h1> Digital Clock {ctime2}</h1>
+    </>
+  )
 }
 
 export default App;
